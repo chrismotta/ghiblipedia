309abc139e1278234744be7710d4ff65cd5675f6
Christian Motta <cm@castinellc.com>
2020-09-12 20:49:37 -0300

    tarea

--- src/script.js
+++ src/script.js
@@ -8,6 +8,8 @@ contenidos.innerHTML = "<div class='titulo-contenidos'>Lista de Películas</div>
 let titulosPeliculas = ["Ponyo", "El viaje de Chihiro", "Mi vecino Totoro", "El Castillo bagabundo", "La princesa Mononoke"];
 console.log("longitud de titulosPeliculas", titulosPeliculas.length);
 
+// TAREA: al div con los titulos agregarle una class y darle algun estilo en style.css
+
 for (let i = 0; i < titulosPeliculas.length; i++){
     contenidos.innerHTML += "<div>" + titulosPeliculas[i] + "</div>";
 }
